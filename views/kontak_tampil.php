46cc1d7e949ee6b1df95c585c86c83d2a023b18e
fikrialfadani2 <fikrialfadani2@gmail.com>
2022-06-10 14:16:22 +0700

    project3

---
+++ views/kontak_tampil.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/maalkhoirot.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>PERPUSTAKAAN MA AL-KHOIROT</strong></center></label>
+                            <p align="center">
+                              Jl. Kh Syuhud Zayyadi no. 01 Karangsuko, Pagelaran<br>
+                              Kabupaten Malang, Jawa Timur, Kode Pos : 65474
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. (0341)879730<br>
+                            Email : ma@alkhoirot.com<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
